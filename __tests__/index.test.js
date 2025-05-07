d3474a83ccf67e0b347084a6405ef42ac1501f5b
Gleb Elenev <elenev@greensight.ru>
2025-05-07 18:02:34 +0300

    feat: relevance

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,26 +1,28 @@
 // @ts-check
 
-import { expect, test } from 'vitest'
+import {expect, describe, test} from 'vitest'
 import search from '../index.js'
 
-const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a Pint!" };
-const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
-const doc3 = { id: 'doc3', text: "I'm your shooter." };
+const doc1 = {id: 'doc1', text: "I can't shoot straight unless I've had a Pint!"};
+const doc2 = {id: 'doc2', text: "Don't shoot shoot shoot that thing at me."};
+const doc3 = {id: 'doc3', text: "I'm your shooter."};
 const docs = [doc1, doc2, doc3];
 
-test('find term', () => {
-  expect(search(docs, 'shoot')).toEqual(['doc1', 'doc2'])
-})
+describe('search', () => {
+  test('empty docs', () => {
+    expect(search([], 'shoot')).toEqual([])
+  })
 
-test('empty docs', () => {
-  expect(search([], 'shoot')).toEqual([])
-})
+  test('empty term', () => {
+    expect(search(docs, '')).toEqual([])
+  })
 
-test('empty term', () => {
-  expect(search(docs, '')).toEqual([])
-})
+  test('clean words', () => {
+    expect(search(docs, 'pint')).toEqual(['doc1'])
+    expect(search(docs, 'pint!')).toEqual(['doc1'])
+  })
 
-test('clean words', () => {
-  expect(search(docs, 'pint')).toEqual(['doc1'])
-  expect(search(docs, 'pint!')).toEqual(['doc1'])
+  test('relevance', () => {
+    expect(search(docs, 'shoot')).toEqual(['doc2', 'doc1'])
+  })
 })
--- src/search.js
+++ src/search.js
@@ -3,15 +3,22 @@
  * @param {string} term
  */
 const search = (docs, term) => {
-
   const cleanTerm = term.match(/\w+/g)?.[0].toLowerCase();
+
   if (!docs || !term || !cleanTerm) return [];
 
   return docs.reduce((acc, {id, text}) => {
     const words = text.match(/\w+/g)?.map(i => i.toLowerCase());
-    if (words && words.includes(cleanTerm)) acc.push(id);
+    if (!words) return acc;
+
+    const filtered = words.filter(word => word === cleanTerm);
+    if (!filtered.length) return acc;
+
+    acc.push({id, count: filtered.length});
     return acc;
-  }, []);
+  }, [])
+    .sort((a, b) => b.count - a.count)
+    .map((item) => item.id);
 }
 
 export default search;
