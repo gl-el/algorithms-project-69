78bfc1631078637b847e54a1e4c5998c8b2e997b
Gleb Elenev <elenev@greensight.ru>
2025-05-07 17:22:34 +0300

    feat: clean word

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,7 @@
 import { expect, test } from 'vitest'
 import search from '../index.js'
 
-const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a Pint!" };
 const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
 const doc3 = { id: 'doc3', text: "I'm your shooter." };
 const docs = [doc1, doc2, doc3];
@@ -18,4 +18,9 @@ test('empty docs', () => {
 
 test('empty term', () => {
   expect(search(docs, '')).toEqual([])
+})
+
+test('clean words', () => {
+  expect(search(docs, 'pint')).toEqual(['doc1'])
+  expect(search(docs, 'pint!')).toEqual(['doc1'])
 })
--- src/search.js
+++ src/search.js
@@ -3,11 +3,13 @@
  * @param {string} term
  */
 const search = (docs, term) => {
-  if (!docs || !term) return [];
+
+  const cleanTerm = term.match(/\w+/g)?.[0].toLowerCase();
+  if (!docs || !term || !cleanTerm) return [];
 
   return docs.reduce((acc, {id, text}) => {
-    const words = text.split(' ');
-    if (words.includes(term.toLowerCase())) acc.push(id);
+    const words = text.match(/\w+/g)?.map(i => i.toLowerCase());
+    if (words && words.includes(cleanTerm)) acc.push(id);
     return acc;
   }, []);
 }
