3285f24e18919d9e276fbdb071af5b491e9da72c
Gleb <g.elenev@gmail.com>
2025-05-10 09:22:55 +0300

    feat: inverted index

--- src/search.js
+++ src/search.js
@@ -1,3 +1,31 @@
+const invertedIndex = {}
+let prevDocsHash = ''
+/**
+ * @param {{id: string, text: string}[]} docs
+ */
+const buildInvertedIndex = (docs) => {
+  const hash = docs.reduce((acc, { id, text }) => acc += `|${id}:${text}`, '')
+  if (hash !== prevDocsHash) {
+    const newIndex = {}
+    prevDocsHash = hash
+    docs.forEach(({ id, text }) => {
+      const words = text.match(/\w+/g)?.map(word => word.toLowerCase()) || []
+      const freqMap = new Map()
+
+      for (const word of words) {
+        freqMap.set(word, (freqMap.get(word) || 0) + 1)
+      }
+
+      for (const [word, count] of freqMap.entries()) {
+        if (!newIndex[word]) newIndex[word] = {}
+        newIndex[word][id] = count
+      }
+    })
+    invertedIndex[hash] = newIndex
+  }
+  return invertedIndex[hash]
+}
+
 /**
  * @param {{id: string, text: string}[]} docs
  * @param {string} term
@@ -5,42 +33,37 @@
 const search = (docs, term) => {
   const cleanTerms = term.match(/\w+/g)?.map(term => term.toLowerCase())
 
-  if (!docs || !term || !cleanTerms?.length) return []
+  if (!docs.length || !term || !cleanTerms?.length) return []
 
   const totalTerms = cleanTerms.length
 
-  return docs.reduce((acc, { id, text }) => {
-    const words = text.match(/\w+/g)
-    if (!words) return acc
+  const index = buildInvertedIndex(docs)
 
-    const wordFreqMap = new Map()
+  const docScores = new Map()
+  const docMatchedTerms = new Map()
 
-    words.forEach((word) => {
-      const key = word.toLowerCase()
-      wordFreqMap.set(key, (wordFreqMap.get(key) || 0) + 1)
-    })
+  for (const term of cleanTerms) {
+    const posting = index[term]
+    if (!posting) continue
 
-    let termFrequencySum = 0
-    let uniqueTermMatches = 0
+    for (const [docId, freq] of Object.entries(posting)) {
+      docScores.set(docId, (docScores.get(docId) || 0) + freq)
 
-    cleanTerms.forEach((term) => {
-      const wordsCount = wordFreqMap.get(term)
-      if (wordsCount) {
-        termFrequencySum += wordsCount || 0
-        uniqueTermMatches += 1
+      if (!docMatchedTerms.has(docId)) {
+        docMatchedTerms.set(docId, new Set())
       }
-    })
+      docMatchedTerms.get(docId).add(term)
+    }
+  }
 
-    const count = termFrequencySum * (uniqueTermMatches / totalTerms)
+  const ranked = []
+  for (const [docId, score] of docScores.entries()) {
+    const uniqueMatches = docMatchedTerms.get(docId).size
+    const finalScore = score * (uniqueMatches / totalTerms)
+    ranked.push({ id: docId, score: finalScore })
+  }
 
-    if (!count) return acc
-
-    acc.push({ id, count })
-
-    return acc
-  }, [])
-    .sort((a, b) => b.count - a.count)
-    .map(({ id }) => id)
+  return ranked.sort((a, b) => b.score - a.score).map(({ id }) => id)
 }
 
 export default search
